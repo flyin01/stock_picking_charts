12b29b1152beed98ce72f841d2cb7f5398b272fe
flyin01 <globaljm@hotmail.com>
2022-05-17 22:57:19 +0200

    initial commit

---
+++ creating_artificial_data.r
@@ -0,0 +1,23 @@
+# Creating artificial data
+
+library(tibble)
+library(ggplot2)
+
+set.seed(42)
+mu <- 0
+sigma <- 0.1
+
+# This is our predefined function
+f <- function(x) 3*x^2 + 2*x + 1
+
+# Adding noise to a point along a defined function is a way to create artificial dataset to test model fitting
+noise <- function(x, scale) {rnorm(n = length(x), mean = 0, sd = scale)}
+add_noise <- function(x, mult, add) {x * (1 + noise(x, mult)) + noise(x, add)}
+
+x <- seq(-2, 2, length.out = 20)
+y <- add_noise(f(x), 0.3, 1.5)
+
+tibble(x=x, y=y) %>%
+  ggplot(aes(x,y)) +
+  geom_point(colour = "dark blue", size = 3) +
+  geom_function(fun = f, colour = "red", size = 0.8)
