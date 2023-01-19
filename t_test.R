bb4abe34799cd29fed7cd5c4c088db2737633199
flyin01 <globaljm@hotmail.com>
2023-01-19 22:44:36 +0100

    minor change

--- t_test.R
+++ t_test.R
@@ -2,36 +2,36 @@
 
 set.seed(42)
 ### rnorm
-group_a <- rnorm(n=20, mean=0, sd=1)
-group_b <- rnorm(n=20, mean=0, sd=3)
+group_a <- rnorm(n = 20, mean = 0, sd = 1)
+group_b <- rnorm(n = 20, mean = 0, sd = 3)
 
 df <- data.frame("group_a" = as.numeric(group_a),
                  "group_b" = as.numeric(group_b))
 
 dplyr::glimpse(df)
 
-t_a = round(mean(df$group_a),5)
-t_b = round(mean(df$group_b),5)
+t_a <- round(mean(df$group_a), 5)
+t_b <- round(mean(df$group_b), 5)
 
 library(tidyverse)
 
 # Plot the two distributions
-df %>% 
-  ggplot(aes(x=group_a)) +
-  geom_histogram(alpha = .25, fill="blue") +
-  geom_vline(xintercept = t_a, color="blue") +
-  geom_text(aes(x=t_a, label=t_a),
-            y=0.5, angle=90, vjust=-0.2, size=3, color="blue") +
-  geom_histogram(aes(x=group_b), alpha=.25, fill="red") +
-  geom_vline(xintercept = t_b, color="red") +
-  geom_text(aes(x=t_b, label=t_b),
-            y=0.5, angle=90, vjust=-0.2, size=3, color="red") +
+df %>%
+  ggplot(aes(x = group_a)) +
+  geom_histogram(alpha = .25, fill = "blue") +
+  geom_vline(xintercept = t_a, color = "blue") +
+  geom_text(aes(x = t_a, label = t_a),
+            y = 0.5, angle = 90, vjust = -0.2, size = 3, color = "blue") +
+  geom_histogram(aes(x = group_b), alpha = .25, fill = "red") +
+  geom_vline(xintercept = t_b, color = "red") +
+  geom_text(aes(x = t_b, label = t_b),
+            y = 0.5, angle = 90, vjust = -0.2, size = 3, color = "red") +
   ggtitle("Historgram of two sammple normal groups with rounded mean on v-lines")
 
 # 1. Welch Two Sample t-test for mean
-res <- t.test(x=df$group_a,
-              y=df$group_b,
-              mu=0, # diff of means
+res <- t.test(x = df$group_a,
+              y = df$group_b,
+              mu = 0, # diff of means
               aternative = "two.sided",
               conf.level = 0.95,
               var.equal = FALSE) # Welch approximation to the degrees of freedom is used
@@ -40,9 +40,9 @@ res
 print(res$p.value)
 
 # 1b. Student Two Sample t-test for mean
-res1 <- t.test(x=df$group_a,
-              y=df$group_b,
-              mu=0, # diff of means
+res1 <- t.test(x = df$group_a,
+              y = df$group_b,
+              mu = 0, # diff of means
               aternative = "two.sided",
               conf.level = 0.95,
               var.equal = TRUE) # Pooled variance is used to estimate the variance
@@ -53,8 +53,8 @@ print(res1$p.value)
 
 
 # 2. KS (Kolmogovor-Smirnov) test - a non-parametric and distribution free test.
-res2 <- ks.test(x=df$group_a,
-                y=df$group_b,
+res2 <- ks.test(x = df$group_a,
+                y = df$group_b,
                 alternative = "two.sided")
 
 
@@ -69,26 +69,26 @@ print(res2$p.value) # 0.03354
 ### lognorm
 #group_a1 <- rlnorm(n=20, meanlog=3, sdlog=10)
 #group_b1 <- rlnorm(n=20, meanlog=8, sdlog=10)
-group_a1 <- rlnorm(n=200, meanlog=0, sdlog=1)
-group_b1 <- rlnorm(n=200, meanlog=0, sdlog=1.1)
+group_a1 <- rlnorm(n = 200, meanlog = 0, sdlog = 1)
+group_b1 <- rlnorm(n = 200, meanlog = 0, sdlog = 1.1)
 
 df2 <- data.frame("group_a1" = as.numeric(group_a1),
                   "group_b1" = as.numeric(group_b1))
 
-t_a1 = round(mean(df2$group_a1),5)
-t_b1 = round(mean(df2$group_b1),5)
+t_a1 <- round(mean(df2$group_a1), 5)
+t_b1 <- round(mean(df2$group_b1), 5)
 
 df2 %>%
   filter(group1_a1 < 3) %>%
-  ggplot(aes(x=group_a1)) +
-  geom_histogram(alpha = .25, fill="blue") +
-  geom_vline(xintercept = t_a1, color="blue") +
-  geom_text(aes(x=t_a1, label=t_a1),
-            y=0.5, angle=90, vjust=-0.2, size=3, color="blue") +
-  geom_histogram(aes(x=group_b1), alpha=.25, fill="red") +
-  geom_vline(xintercept = t_b1, color="red") +
-  geom_text(aes(x=t_b1, label=t_b1),
-            y=0.5, angle=90, vjust=-0.2, size=3, color="red") +
+  ggplot(aes(x = group_a1)) +
+  geom_histogram(alpha = .25, fill = "blue") +
+  geom_vline(xintercept = t_a1, color = "blue") +
+  geom_text(aes(x = t_a1, label = t_a1),
+            y = 0.5, angle = 90, vjust = -0.2, size = 3, color = "blue") +
+  geom_histogram(aes(x = group_b1), alpha = .25, fill = "red") +
+  geom_vline(xintercept = t_b1, color = "red") +
+  geom_text(aes(x = t_b1, label = t_b1),
+            y = 0.5, angle = 90, vjust = -0.2, size = 3, color = "red") +
   ggtitle("Historgram of two sample lognormal groups with rounded mean on v-lines")
 
 # exp log vals
@@ -96,19 +96,18 @@ df2 <- df2 %>%
   mutate(group_a1_exp = exp(group_a1)) %>%
   mutate(group_b1_exp = exp(group_b1))
 
-t_a1_exp = round(mean(df2$group_a1_exp),5)
+t_a1_exp <- round(mean(df2$group_a1_exp), 5)
 
 df2 %>%
   filter(group_a1 < 3) %>%
   filter(group_b1 < 3) %>%
-  ggplot(aes(x=group_a1)) +
-  geom_histogram(alpha = .25, fill="blue") +
-  geom_vline(xintercept = t_a1, color="blue") +
-  geom_text(aes(x=t_a1, label=t_a1),
-            y=0.5, angle=90, vjust=-0.2, size=3, color="blue") +
-  geom_histogram(aes(x=group_a1_exp), alpha=.25, fill="red") +
-  geom_vline(xintercept = t_a1_exp, color="red") +
-  geom_text(aes(x=t_a1_exp, label=t_a1_exp),
-            y=0.5, angle=90, vjust=-0.2, size=3, color="red") +
+  ggplot(aes(x = group_a1)) +
+  geom_histogram(alpha = .25, fill = "blue") +
+  geom_vline(xintercept = t_a1, color = "blue") +
+  geom_text(aes(x = t_a1, label = t_a1),
+            y = 0.5, angle = 90, vjust = -0.2, size = 3, color = "blue") +
+  geom_histogram(aes(x = group_a1_exp), alpha = .25, fill = "red") +
+  geom_vline(xintercept = t_a1_exp, color = "red") +
+  geom_text(aes(x = t_a1_exp, label = t_a1_exp),
+            y = 0.5, angle = 90, vjust = -0.2, size = 3, color = "red") +
   ggtitle("Historgram of two sample lognormal groups with rounded mean on v-lines")
-
