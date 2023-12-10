a652a62eb1fe432cfec3e638106cf4d8145f4aa2
flyin01 <globaljm@hotmail.com>
2023-12-10 22:10:45 +0100

    added plots

--- distance_metrics.R
+++ distance_metrics.R
@@ -1,7 +1,7 @@
 ####################
 # Distance metrics #
 ####################
-
+library(ggplot2)
 
 ## Input Data 
 # Create table with 2 observations with coordinates in a 2D space
@@ -11,6 +11,12 @@ df <- data.frame(X = as.numeric(c(0,9)),
 
 df
 
+# Plot the positions of the 2 observations
+ggplot(df, aes(x = X, y = X)) + 
+  geom_point() +
+  # Assuming a 40x60 field
+  lims(x = c(-20,20), y = c(-20, 20))
+
 ### Euclidean distance - formula is the same as the Hypothenuse of a triangle 
 ### formed by the coordinates of the base and height
 ### This is the fundamental idea of calculating a measure of dissimilarity of two observations
@@ -18,7 +24,7 @@ df
 
 ## Alt 1
 # Manually calculate the distance
-distance <- sqrt( ( df[1,1] - df[2,1] )^2 + (df[1,2] - df[2,2])^2 )
+distance <- sqrt( ( df[1,1] - df[2,1])^2 + (df[1,2] - df[2,2])^2 )
 distance
 
 
@@ -34,7 +40,7 @@ calculate_eucledian <- function(df) {
 
 }
 
-# Apply custom functio
+# Apply custom function
 result <- calculate_eucledian(df)
 cat("The Eucledian distance is:", result)
 
@@ -55,5 +61,11 @@ df2
 # apply dist() on df2 with 3 observations
 result2 <- dist(df2, method = "euclidean")
 result2
+
+# Plot the positions of the 3 observations
+ggplot(df2, aes(x = X, y = X)) + 
+  geom_point() +
+  # Assuming a 40x60 field
+  lims(x = c(-20,20), y = c(-20, 20))
 
 
