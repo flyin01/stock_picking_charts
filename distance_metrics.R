ace0bffac9529855f7244a0f768f6f11af0b6baf
flyin01 <globaljm@hotmail.com>
2023-12-10 21:53:31 +0100

    initial commit

---
+++ distance_metrics.R
@@ -0,0 +1,59 @@
+####################
+# Distance metrics #
+####################
+
+
+## Input Data 
+# Create table with 2 observations with coordinates in a 2D space
+df <- data.frame(X = as.numeric(c(0,9)),
+                 Y = as.numeric(c(0,12))
+                )
+
+df
+
+### Euclidean distance - formula is the same as the Hypothenuse of a triangle 
+### formed by the coordinates of the base and height
+### This is the fundamental idea of calculating a measure of dissimilarity of two observations
+
+
+## Alt 1
+# Manually calculate the distance
+distance <- sqrt( ( df[1,1] - df[2,1] )^2 + (df[1,2] - df[2,2])^2 )
+distance
+
+
+## Alt 2
+# Define a custom distance function that takes as input a df with two numeric columns
+calculate_eucledian <- function(df) {
+  
+  # Col names of a data frame with 2 columns of numeric values
+  
+  distance <- sqrt( (df[1,1] - df[2,1])^2 + (df[1,2] - df[2,2])^2 )
+  
+  return(distance)
+
+}
+
+# Apply custom functio
+result <- calculate_eucledian(df)
+cat("The Eucledian distance is:", result)
+
+
+## Alt 3
+# Use R built in function dist()
+
+result <- dist(df, method = "euclidean")
+result
+
+
+# Create data with 3 observations
+df2 <- data.frame(X = as.numeric(c(0,9,-2)),
+                  Y = as.numeric(c(0,12,19))
+                  )
+df2
+
+# apply dist() on df2 with 3 observations
+result2 <- dist(df2, method = "euclidean")
+result2
+
+
